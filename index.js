1618a1af0bf9e544ce8754cb49655f549bcdae91
Elbert Wang <elbert.wang@wsj.com>
2019-12-10 20:29:43 -0500

    better update

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ const container = document.querySelector(".scroller");
 const scenes = document.querySelectorAll(".scene");
 const images = document.querySelectorAll(".photo");
 console.log(images);
-console.log(images[0]);
-console.log(images[1]);
-console.log(images[2]);
-console.log(images[3]);
 
 const scroller = new window.Scroller({
   container: container,
@@ -13,18 +9,18 @@ const scroller = new window.Scroller({
 });
 
 scroller.on("scene:enter", d => {
-  const currentIndex = d.index;
-  console.log(`currentIndex: ${d.index}`);
-  images.forEach((d, i) => {
-    console.log(`loop index: ${i}`);
-    if (i == currentIndex) {
-      images[i].classList.add("active");
-      images[i].classList.remove("inactive");
-    } else {
-      images[i].classList.add("inactive");
-      images[i].classList.remove("active");
-    }
-  });
+  images[d.index].classList.add("active");
+});
+
+scroller.on("scene:exit", d => {
+  console.log(d.isScrollingDown);
+  if (d.index != 0 && !d.isScrollingDown) {
+    images[d.index].classList.remove("active");
+  }
+});
+
+window.addEventListener("load", event => {
+  images[0].classList.add("active");
 });
 
 // starts up the IntersectionObserver
